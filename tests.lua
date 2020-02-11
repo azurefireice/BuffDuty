8b23eb8a3e7ef43e7a5a030c276f1de4735f8f20
AzureFireIce <AzureFireIce@gmail.com>
2020-02-11 23:01:12 +0200

    Releasing version - 1.2.0 - Excluded players
    Added functionality of excluding players, it was done to address case when due to talent distribution, player is not good for buffing/dispelling or other reasons.
    Added a new argument to /buffduty command to exclude players.
    Added Readme section and examples describing how to exclude players.
    Added screenshots with examples to repository section `docs`.
    Fixed title message when posting to channel to be universal and applicable not only to mages.
    Updated tests to support excluded players.
    Changed `mages` term to `players` term in tests.

--- tests.lua
+++ tests.lua
@@ -2,14 +2,14 @@
 --SETUP TESTS--
 ---------------
 local mock_party_size
-local mock_mages_num
+local mock_players_num
 
 local CLASS_TO_TEST = "DRUID"
 local CLASS_TO_TEST_REF = CLASS_TO_TEST:lower()
 
 
 local mock_getNameClass = function (self, idx)
-    if (idx <= mock_mages_num) then
+    if (idx <= mock_players_num) then
         return CLASS_TO_TEST_REF .. idx, CLASS_TO_TEST
     end
     return "NotA" .. CLASS_TO_TEST_REF, "!" .. CLASS_TO_TEST
@@ -24,6 +24,22 @@ function BuffDuty:OnInitialize()
     self:RegisterChatCommand(test_command, "TestCommand")
 end
 
+local function dump(o)
+    if type(o) == 'table' then
+        local s = '{ '
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then
+                k = '"' .. k .. '"'
+            end
+            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
+end
+
+
 ---------------
 -- RUN TESTS --
 ---------------
@@ -32,51 +48,60 @@ local function test()
     BuffDuty["getNameClass"] = mock_getNameClass
     ---------------
     mock_party_size = 0
-    mock_mages_num = 0
+    mock_players_num = 0
     local duties = BuffDuty:getDutiesTable(CLASS_TO_TEST)
-    assert(#duties == 0, "Duties are not empty, while group size and mages are 0")
+    assert(#duties == 0, "Duties are not empty, while group size and players are 0")
     ---------------
     mock_party_size = 10
-    mock_mages_num = 0
+    mock_players_num = 0
     duties = BuffDuty:getDutiesTable(CLASS_TO_TEST)
-    assert(#duties == 0, "Duties are not empty, while no mages to buff")
+    assert(#duties == 0, "Duties are not empty, while no players to buff")
     ---------------
     mock_party_size = 10
-    mock_mages_num = 1
+    mock_players_num = 1
     duties = BuffDuty:getDutiesTable(CLASS_TO_TEST)
     print("Duties : " .. dump(duties))
     key, value = next(duties)
     assert(key == CLASS_TO_TEST_REF .. "1", "Duties are empty!")
     key, value = next(duties, key)
-    assert(key == nil, "Duties cunt is > 2 while mage is one!")
+    assert(key == nil, "Duties cunt is > 2 while player is one!")
     ---------------
     mock_party_size = 10
-    mock_mages_num = 2
+    mock_players_num = 2
     duties = BuffDuty:getDutiesTable(CLASS_TO_TEST)
     key, value = next(duties)
     assert(key == CLASS_TO_TEST_REF .. "2", "Duties are empty!")
     key, value = next(duties, key)
-    assert(key == CLASS_TO_TEST_REF .. "1", "Duties are < 2 while mages are 2!")
+    assert(key == CLASS_TO_TEST_REF .. "1", "Duties are < 2 while players are 2!")
     key, value = next(duties, key)
-    assert(key == nil, "Duties are > 2 while mages are 2!")
+    assert(key == nil, "Duties are > 2 while players are 2!")
     ---------------
     mock_party_size = 40
-    mock_mages_num = 8
+    mock_players_num = 8
     duties = BuffDuty:getDutiesTable(CLASS_TO_TEST)
     local duties_count = 0
     for _, _ in pairs(duties) do
         duties_count = duties_count + 1
     end
-    assert(duties_count == 8, "Duties are not 8 while mages are 8!")
+    assert(duties_count == 8, "Duties are not 8 while players are 8!")
     ---------------
     mock_party_size = 40
-    mock_mages_num = 12
+    mock_players_num = 12
     duties = BuffDuty:getDutiesTable(CLASS_TO_TEST)
     duties_count = 0
     for _, _ in pairs(duties) do
         duties_count = duties_count + 1
     end
-    assert(duties_count == 8, "Duties are not 8 while mages are 12!")
+    assert(duties_count == 8, "Duties are not 8 while players are 12!")
+    ---------------
+    mock_party_size = 40
+    mock_players_num = 8
+    duties = BuffDuty:getDutiesTable(CLASS_TO_TEST, { "druid1", "druid2" })
+    duties_count = 0
+    for _, _ in pairs(duties) do
+        duties_count = duties_count + 1
+    end
+    assert(duties_count == 6, "Duties are not 6 while players are 8 and 2 are excluded!")
 end
 
 
