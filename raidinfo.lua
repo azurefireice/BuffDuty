72ef9716f280e78e068d6aefd618ef5cf1c18864
Byron <byron.miles@gmail.com>
2020-05-18 20:21:46 +1000

    Return seperate tables for raid_info and class_players

--- raidinfo.lua
+++ raidinfo.lua
@@ -14,55 +14,52 @@ local function getRaidMemberCount()
     return GetNumGroupMembers() -- WOW API: https://wowwiki.fandom.com/wiki/API_GetNumGroupMembers
 end
 
--- Scans the raid returns the following structed information:
--- member_count - total number of players in the raid
--- groups[i] - true if group 'i' is a valid raid group
--- groups.count - the number of valid groups in the raid
--- groups.max - the highest numbered raid group; e.g. 8 for a full raid
--- groups.min - the lowest numbered raid group; e.g. 1 in most cases
--- players.count - the number of duty assignable players in the raid
--- players.map - a map of duty assignable players index by name
--- players.map[name].idx - the index of the player, from 1 to count
--- players.map[name].name - the name of the player
--- players.map[name].group - the group the player is in
+-- Scans the raid returns the following two data structures:
+-- raid_info.member_count - total number of players in the raid
+-- raid_info.group_count - the number of valid groups in the raid
+-- raid_info.group_min - the lowest numbered raid group; e.g. 1 in most cases
+-- raid_info.group_max - the highest numbered raid group; e.g. 8 for a full raid
+-- raid_info.groups[i] - a list of valid groups, indexed by group number (1 to 8)
+-- class_players.count - the number of duty assignable players in the raid
+-- class_players.map - a map of duty assignable players, indexed by name
+-- class_players.map[name].idx - the index of the player, from 1 to count
+-- class_players.map[name].name - the name of the player
+-- class_players.map[name].group - the group the player is in
 function RaidInfo.Scan(class, excluded)
     local raid_info = {}
     raid_info.member_count = getRaidMemberCount()
+    raid_info.group_count = 0
+    raid_info.group_min = 8 -- Start high so logic works
+    raid_info.group_max = 1 -- Start low so logic works
+    raid_info.groups = {}
 
-    local group_info = {[1] = false, [2] = false, [3] = false, [4] = false, [5] = false, [6] = false, [7] = false, [8] = false}
-    group_info.max = 1 -- Start low so logic works
-    group_info.min = 8 -- Start high so logic works
-    group_info.count = 0
-    raid_info.groups = group_info
-
-    local player_info = {}
-    player_info.map = {}
-    player_info.count = 0
-    raid_info.players = player_info
-
+    local class_players = {}
+    class_players.count = 0
+    class_players.map = {}
+    
     for i = 1, raid_info.member_count do
-        local name, class, group = getNameClassGroup(i)
-        -- Setup group info
-        if not group_info[group] then
-            group_info[group] = true
-            group_info.count = group_info.count + 1
-            if group > group_info.max then
-                tbl_groups.max = group
+        local player_name, player_class, player_group = getNameClassGroup(i)
+        -- Setup raid info
+        if not raid_info.groups[player_group] then
+            raid_info.groups[player_group] = true
+            raid_info.group_count = raid_info.group_count + 1
+            if player_group < raid_info.group_min then
+                raid_info.group_min = player_group
             end
-            if group < group_info.min then
-                group_info.min = group
+            if player_group > raid_info.group_max then
+                raid_info.group_max = player_group
             end
         end
-        -- Setup player info
-        if name and class == class and (not utils.containsName(excluded, name)) then
-            player_info.count = player_info.count + 1
-            player_info.map[name] = {
-                idx = player_info.count,
-                name = name,
-                group = group,
+        -- Setup class players
+        if player_name and player_class == class and (not utils.containsName(excluded, player_name)) then
+            class_players.count = class_players.count + 1
+            class_players.map[player_name] = {
+                idx = class_players.count,
+                name = player_name,
+                group = player_group,
             }
         end
     end
 
-    return raid_info
+    return raid_info, class_players
 end
