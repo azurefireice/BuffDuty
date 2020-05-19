660c0be2ba6876f9bde5c4d17b02fd05ce69fdf0
Byron <byron.miles@gmail.com>
2020-05-19 13:29:17 +1000

    Changes for Assign logic

--- logic_assign.lua
+++
@@ -1,9 +0,0 @@
-local Logic = BuffDuty.Logic
-
------------- ASSIGN LOGIC ------------
-local assignLogic = {}
-Logic[Logic.Type.ASSIGN] = assignLogic
-
-function assignLogic.generateDutyMap(cmd, raid_info, class_players)
-    -- TODO
-end
