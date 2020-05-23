f75d04a0add199ac1db7ed271992929e129280fe
Byron <byron.miles@gmail.com>
2020-05-23 16:06:40 +1000

    Fixed to use new localised channel types

--- output.lua
+++ output.lua
@@ -51,7 +51,7 @@ function BuffDuty.printDuties(cmd, channel_type, channel_id, duty_table)
     -- Only a single player has been assigned buffing duty
     if duty_count == 1 then
         local player_name, player_macros = next(duty_table)
-        if channel_type == BuffDuty.WHISPER_CHANNEL_TYPE then
+        if channel_type == BuffDuty.CHANNELS.WHISPER then
             local single_whisper = cmd.single_whisper or messages.single_whisper
             player_macros["class"] = duty_macros["class"] -- Add the "class" to the player macros
             single_whisper = macroReplace(single_whisper, player_macros)
@@ -59,7 +59,7 @@ function BuffDuty.printDuties(cmd, channel_type, channel_id, duty_table)
                 printInfoMessage(single_whisper, "ffff78ea")
             else
                 single_whisper = string.format(messages.message_title, single_whisper)
-                SendChatMessage(single_whisper, BuffDuty.WHISPER_CHANNEL_TYPE, nil, player_name)
+                SendChatMessage(single_whisper, channel_type, nil, player_name)
             end
         else -- Public channel
             local single_message = string.format(messages.message_title, cmd.single_message or messages.single_message)
@@ -71,7 +71,7 @@ function BuffDuty.printDuties(cmd, channel_type, channel_id, duty_table)
     end
 
     -- Multiple players have been assinged buffing duty
-    if channel_type == BuffDuty.WHISPER_CHANNEL_TYPE then 
+    if channel_type == BuffDuty.CHANNELS.WHISPER then
         local whisper_message = cmd.duty_whisper or messages.duty_whisper
         for player_name, player_macros in pairs(duty_table) do -- For each player
             local duty_whisper = macroReplace(whisper_message, player_macros)
@@ -79,7 +79,7 @@ function BuffDuty.printDuties(cmd, channel_type, channel_id, duty_table)
                 printInfoMessage(duty_whisper, "ffff78ea")
             else
                 whisper_message = string.format(messages.message_title, whisper_message)
-                SendChatMessage(duty_whisper, BuffDuty.WHISPER_CHANNEL_TYPE, nil, player_name)
+                SendChatMessage(duty_whisper, channel_type, nil, player_name)
             end
         end
     else -- Public channel
